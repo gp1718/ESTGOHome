6224f486f1f66fd9f074f67db4236337d4093301
ruialmeida96 <34514089+ruialmeida96@users.noreply.github.com>
2018-01-31 21:13:56 +0000

    Tentativas de Implementar o envio de Emails

--- resources/pages/forgot.php
+++ resources/pages/forgot.php
@@ -5,8 +5,42 @@
   <h6>Introduza em baixo o email associado à sua conta, e clique em "Recuperar". <br>Receberá posteriormente um email onde poderá facilmente criar uma nova palavra passe.</h6>
   <br><input type="email" id="emailrecupera" name="emailrecupera" placeholder="Insira o seu e-mail" autofocus="" required size="32" minlength="8" maxlength="254">
   <br><br>
-
-  <a class="btn btn-success" href="#" role="button">Recuperar</a>
+<form method="post">
+  <a class="btn btn-success" href='?action=forgot?hello=true' role="button" id="recupera" name="recupera"  >Recuperar</a>
+</form>
   <br>
   <br>
 </div>
+
+
+<?php
+  /*function runMyFunction() {
+    echo 'I just ran a php function';
+  }
+*/
+
+/*
+if(isset($_GET["hello"])) {
+
+alert("ola");
+}*/
+
+
+if(array_key_exists('recupera',$_POST)){
+   testfun();
+}
+
+function testfun(){
+   echo "Your test function on button click is working";
+}
+
+    function enviaemail(){
+      $to      = 'rui_almeida_96@hotmail.com';
+      $subject = 'OLA';
+      $message = 'hello';
+      $headers = 'From: patroll.cs1.6@gmail.com' . "\r\n" .
+          'Reply-To: patroll.cs1.6@gmail.com' . "\r\n" .
+          'X-Mailer: PHP/' . phpversion();
+      mail($to, $subject, $message, $headers);
+    }
+?>
