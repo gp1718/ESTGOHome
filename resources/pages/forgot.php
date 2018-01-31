cc2fee226844482a01159ded5be8007cd0d79b5e
ruialmeida96 <34514089+ruialmeida96@users.noreply.github.com>
2018-01-31 17:48:10 +0000

    Estrutura visual de forgot.php feita

--- resources/pages/forgot.php
+++ resources/pages/forgot.php
@@ -1,9 +1,12 @@
-<!--<p> pagina forgot <p>
-<br>
-<div>
+<div style="text-align: center;">
 
- <input type="email" name="email">
+    <h2>Recuperar Palavra-Passe</h2><br>
 
+  <h6>Introduza em baixo o email associado à sua conta, e clique em "Recuperar". <br>Receberá posteriormente um email onde poderá facilmente criar uma nova palavra passe.</h6>
+  <br><input type="email" id="emailrecupera" name="emailrecupera" placeholder="Insira o seu e-mail" autofocus="" required size="32" minlength="8" maxlength="254">
+  <br><br>
 
+  <a class="btn btn-success" href="#" role="button">Recuperar</a>
+  <br>
+  <br>
 </div>
--->
