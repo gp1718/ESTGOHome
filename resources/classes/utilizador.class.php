da2e05ee4778e68fb7a365c1e2f84de159d52ef7
Pedro Calado <pcalado99@gmail.com>
2018-01-31 11:31:06 +0000

    Criação da classe Utilizador.

--- resources/classes/utilizador.class.php
+++ resources/classes/utilizador.class.php
@@ -1,4 +1,35 @@
 <?php
 class Utilizador {
+  private $id, $nome, $email, $password, $contacto, $tipo, $estado;
 
+  public function __construct($id, $nome, $email, $password, $contacto, $tipo, $estado) {
+		$this->id = $id;
+		$this->nome = $nome;
+		$this->email = $email;
+		$this->password = $password;
+    $this->contacto = $contacto;
+		$this->tipo = $tipo;
+		$this->estado = $estado;
+	}
+	public function get_id() {
+		return $this->id;
+	}
+	public function get_nome() {
+		return $this->nome;
+	}
+	public function get_email() {
+		return $this->email;
+	}
+	public function get_password() {
+		return $this->password;
+	}
+	public function get_contacto() {
+		return $this->contacto;
+	}
+	public function get_tipo() {
+		return $this->tipo;
+	}
+	public function get_estado() {
+		return $this->estado;
+	}
 }
