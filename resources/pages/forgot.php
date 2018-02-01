f84b944ab8d21d61a36206804e614d9be0016a83
ruialmeida96 <34514089+ruialmeida96@users.noreply.github.com>
2018-02-01 19:32:58 +0000

    Ediçao da pagina forgot.php e criaçao de uma nova pagina editarinfo.php

--- resources/pages/forgot.php
+++ resources/pages/forgot.php
@@ -1,22 +1,22 @@
 <div style="text-align: center;">
 
-    <h2>Recuperar Palavra-Passe</h2><br>
+  <h2>Recuperar Palavra-Passe</h2><br>
 
   <h6>Introduza em baixo o email associado à sua conta, e clique em "Recuperar". <br>Receberá posteriormente um email onde poderá facilmente criar uma nova palavra passe.</h6>
   <br><input type="email" id="emailrecupera" name="emailrecupera" placeholder="Insira o seu e-mail" autofocus="" required size="32" minlength="8" maxlength="254">
   <br><br>
-<form method="post">
-  <a class="btn btn-success" href='?action=forgot?hello=true' role="button" id="recupera" name="recupera"  >Recuperar</a>
-</form>
+  <form method="post">
+    <a class="btn btn-success" href='?action=forgot?hello=true' role="button" id="recupera" name="recupera"  >Recuperar</a>
+  </form>
   <br>
   <br>
 </div>
 
 
 <?php
-  /*function runMyFunction() {
-    echo 'I just ran a php function';
-  }
+/*function runMyFunction() {
+echo 'I just ran a php function';
+}
 */
 
 /*
@@ -27,20 +27,20 @@ alert("ola");
 
 
 if(array_key_exists('recupera',$_POST)){
-   testfun();
+  testfun();
 }
 
 function testfun(){
-   echo "Your test function on button click is working";
+  echo "Your test function on button click is working";
 }
 
-    function enviaemail(){
-      $to      = 'rui_almeida_96@hotmail.com';
-      $subject = 'OLA';
-      $message = 'hello';
-      $headers = 'From: patroll.cs1.6@gmail.com' . "\r\n" .
-          'Reply-To: patroll.cs1.6@gmail.com' . "\r\n" .
-          'X-Mailer: PHP/' . phpversion();
-      mail($to, $subject, $message, $headers);
-    }
+function enviaemail(){
+  $to      = 'rui_almeida_96@hotmail.com';
+  $subject = 'OLA';
+  $message = 'hello';
+  $headers = 'From: patroll.cs1.6@gmail.com' . "\r\n" .
+  'Reply-To: patroll.cs1.6@gmail.com' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+  mail($to, $subject, $message, $headers);
+}
 ?>
