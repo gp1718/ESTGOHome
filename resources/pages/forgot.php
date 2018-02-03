cee6815976334233e9658966f712ccc4e9c7c761
ruialmeida96 <34514089+ruialmeida96@users.noreply.github.com>
2018-02-03 17:49:53 +0000

    Criaçao da pagina forgot_thanks.php

--- resources/pages/forgot.php
+++ resources/pages/forgot.php
@@ -5,50 +5,53 @@ if(isset($_SESSION['U_ID'],$_SESSION['U_TIPO'])){
 	header("Location: $url");
   die();
 }
+
+
+
+//Envio de email
+if(isset($_POST['emailrecupera']) ){
+	if(!empty($_POST['emailrecupera'])){
+		$email = $_POST['emailrecupera'];
+		if(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+			echo 'email invalido';
+		}else{
+			$subject = "Recuperaçao Password";
+			$message = "<p>Enviado</p>";
+			$from = "response@mywebsite.com";
+			$headers = "From: $from" ."\r\n";
+
+			// Send email
+			if(mail($email,$subject,$message,$headers)){
+				header('location ?action=forgot');
+			};
+		}
+
+	}else{
+		echo 'nao introduziu os campos todos';
+	}
+}else{
+echo 'not ok';
+}
+
+
+
+
+
+
 ?>
 <div style="text-align: center;">
 
   <h2>Recuperar Palavra-Passe</h2><br>
 
-  <h6>Introduza em baixo o email associado à sua conta, e clique em "Recuperar". <br>Receberá posteriormente um email onde poderá facilmente criar uma nova palavra passe.</h6>
-  <br><input type="email" id="emailrecupera" name="emailrecupera" placeholder="Insira o seu e-mail" autofocus="" required size="32" minlength="8" maxlength="254">
-  <br><br>
-  <form method="post">
-    <a class="btn btn-success" href='?action=forgot?hello=true' role="button" id="recupera" name="recupera"  >Recuperar</a>
-  </form>
+
+	<form method="post" action="">
+		<h6>Introduza em baixo o email associado à sua conta, e clique em "Recuperar". <br>Receberá posteriormente um email onde poderá facilmente criar uma nova palavra passe.</h6>
+	  <br><input type="email" id="emailrecupera" name="emailrecupera" placeholder="Insira o seu e-mail" autofocus="" required size="32" minlength="8" maxlength="254">
+	  <br><br>
+	    <!--<a class="btn btn-success" href='?action=forgot_thanks' role="button" id="recupera" name="recupera"  >Recuperar</a>-->
+			<input class="btn btn-success" id="recupera" name="recupera" type="submit" value="Recuperar">
+	</form>
+
   <br>
   <br>
 </div>
-
-
-<?php
-/*function runMyFunction() {
-echo 'I just ran a php function';
-}
-*/
-
-/*
-if(isset($_GET["hello"])) {
-
-alert("ola");
-}*/
-
-
-if(array_key_exists('recupera',$_POST)){
-  testfun();
-}
-
-function testfun(){
-  echo "Your test function on button click is working";
-}
-
-function enviaemail(){
-  $to      = 'rui_almeida_96@hotmail.com';
-  $subject = 'OLA';
-  $message = 'hello';
-  $headers = 'From: patroll.cs1.6@gmail.com' . "\r\n" .
-  'Reply-To: patroll.cs1.6@gmail.com' . "\r\n" .
-  'X-Mailer: PHP/' . phpversion();
-  mail($to, $subject, $message, $headers);
-}
-?>
---
+++ resources/pages/forgot_thanks.php
@@ -0,0 +1,1 @@
+<p> agradecimento</p>
